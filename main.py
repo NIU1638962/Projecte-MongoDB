1c10b190164fa208d505fc8c532a1de810673ca3
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2023-03-24 14:13:26 +0100

    Update 20230324b.
    Bucle principal teóricamente listo con la lectura de todos los argumentos.
    Se ha añadido la funcón para conectarse a la base de datos, crear collecciones y añadir los datos de la colleción Artista. Falta hacer las funciones para añadir todos los demás datos d elas otras dos colecciones.
    Se ha reformateado el archivo options.py para mejorar su lectura, además cambio a la opción del nombre de la base de datos, de "-db". a "--db".

--- main.py
+++ main.py
@@ -4,24 +4,129 @@ Created on Fri Mar 10 10:47:53 2023
 
 @author: JoelT
 """
-from sys import argv
-from pymongo import MongoClient
-import pandas
+import pymongo
+import pandas as pd
+from options import Options
 
-try:
-    file = argv[1]
 
-    DSN = "mongodb://{}:{}".format('dcccluster.uab.es', 8194)
-    conn = MongoClient(DSN)
-    bd = conn['Projecte']
+def connect_database(database_name: str, local_test: bool = False) -> pymongo.database.Database:
+    """
+    Connects to the database and returns the pymongo object.
+
+    Parameters
+    ----------
+    database_name : str
+        Name of the database.
+    local_test : bool, optional
+        If true connects to the local database instead of the server. The default is False.
+
+    Returns
+    -------
+    pymongo.database.Database
+        pymongo database object.
+
+    """
+    if local_test:
+        dsn = "mongodb://{}:{}".format('localhost', 27017)
+    else:
+        dsn = "mongodb://{}:{}".format('dcccluster.uab.es', 8194)
+    conn = pymongo.MongoClient(dsn)
+    return conn[database_name]
+
+
+def delete_database(db: pymongo.database.Database) -> None:
+    """
+    Deletes all documents and collections of the database.
+
+    Parameters
+    ----------
+    db : pymongo.database.Database
+        pymongo database object containing the connection to the database that
+        wants to deleted.
+
+    Returns
+    -------
+    None
+
+    """
+    for collection_name in db.list_collection_names():
+        db.get_collection(collection_name).drop()
+
+
+def get_collection(db: pymongo.database.Database, collection_name: str) -> pymongo.collection.Collection:
+    """
+    Get and return the pymongo collection object given a database, if doesn't
+    exist creates it and if exists deletes all documents on it.
+
+    Parameters
+    ----------
+    db : pymongo.database.Database
+        pymongo database object containing the connection to the database.
+    collection_name : str
+        Name of the collection we want to obtain the object off.
+
+    Returns
+    -------
+    pymongo.collection.Collection
+        pymongo collection object containing the connection to the database collection.
+
+    """
     try:
-        coll = bd.create_collection("MongoDB")
-    except:
-        coll = bd["MongoDB"]
-    coll.drop()
+        collection = db.create_collection(collection_name)
+    except pymongo.errors.CollectionInvalid:
+        collection = db.get_collection(collection_name)
+        collection.drop()
+    return collection
 
-    transactions = pandas.read_csv(file)
-    coll.insert_many(transactions.to_dict("records"))
 
-except IndexError:
-    print("Error: No s'ha passat argument.")
+def add_data(db: pymongo.database.Database, file_name: str) -> None:
+    """
+    Adds to the database all data stored in the given file.
+
+    Parameters
+    ----------
+    db : pymongo.database.Database
+        pymongo database object containing the connection to the database.
+    file_name : str
+        Name of the file containing all the data to add to the database.
+
+    Returns
+    -------
+    None
+
+    """
+    add_artists(db, file_name)
+
+
+def add_artists(db: pymongo.database.Database, file_name: str) -> None:
+    """
+    Adds all the artist in the given file to the "Artista" collection of the
+    database.
+
+    Parameters
+    ----------
+    db : pymongo.database.Database
+        pymongo database object containing the connection to the database.
+    file_name : str
+        Name of the file containing all the data to add to the database.
+
+    Returns
+    -------
+    None
+
+    """
+    artista_raw_data = pd.read_excel(file_name, "Artistes")
+    artista_formated_data = [{"_id": row["Nom_artistic"], "nom": row["nom"],
+                              "cognoms": row["cognoms"],
+                              "data_naixement": row["data_naix"],
+                              "pais": row["pais"]} for index, row in artista_raw_data.iterrows()]
+    get_collection(db, "Artista").insert_many(artista_formated_data)
+
+
+if __name__ == "__main__":
+    arguments = Options().parse()
+    db = connect_database(arguments.db, local_test=True)
+    if arguments.delete_all:
+        delete_database(db)
+    elif arguments.fileName:
+        add_data(db, arguments.fileName)
--- options.py
+++ options.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
 """
     Parse input arguments
-    
+
     Original author:
         __author__ = 'Oriol Ramos Terrades'
         __email__ = 'oriolrt@cvc.uab.cat'
+
 """
 
-import argparse
 
-from argparse import HelpFormatter
+from argparse import ArgumentParser, HelpFormatter
 from operator import attrgetter
 
+
 class SortingHelpFormatter(HelpFormatter):
-    
+
     def add_arguments(self, actions):
         actions = sorted(actions, key=attrgetter('option_strings'))
         super(SortingHelpFormatter, self).add_arguments(actions)
 
-class Options(argparse.ArgumentParser):
+
+class Options(ArgumentParser):
 
     def __init__(self):
         # MODEL SETTINGS
-        super().__init__(description="This script inserts data into a mongo-powered database that stores book information", formatter_class=SortingHelpFormatter)
+        super().__init__(description="This script inserts data into a mongo-powered database that stores book information",
+                         formatter_class=SortingHelpFormatter)
         # Positional arguments
-        super().add_argument('-f','--fileName', type=str, help='EXCEL File where data is stored.')
-        super().add_argument('-db', type=str, help='Select the DB to perform the operations')
+        super().add_argument('-f', '--fileName', type=str,
+                             help='EXCEL File where data is stored.')
+        super().add_argument('--db', type=str, required=True,
+                             help='Select the database to perform the operations')
         # Optional arguments
-        super().add_argument('--delete-all', help='Delete all files from the selected DB.', action="store_true")
+        super().add_argument('--delete-all',
+                             help='Delete all files from the selected DB.', action="store_true")
 
     def parse(self):
         return super().parse_args()
-
