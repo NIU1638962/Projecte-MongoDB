7a81cf3ab93fc4d34d3480ece00787f89cd3d366
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2023-03-24 09:15:10 +0100

    Update 20230324a.
    Lector argumento programa.
    Continuar programa en la línea 13.

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ Created on Fri Mar 10 10:47:53 2023
 
 @author: JoelT
 """
+from sys import argv
+
+try:
+    file = argv[1]
+
+    # Continuar programa aquí debajo
+
+
+except IndexError:
+    print("Error, no s'ha passat argument.")
