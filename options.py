5089bd378445fe174d17b57972870fa521b075a3
WorthlessProgrammer <95526357+WorthlessProgrammer@users.noreply.github.com>
2023-03-24 09:55:54 +0100

    Update options.py

--- options.py
+++ options.py
@@ -21,7 +21,7 @@ class Options(argparse.ArgumentParser):
 
     def __init__(self):
         # MODEL SETTINGS
-        super().__init__(description="This script inserts data into a scheme previously created for outlier detection experiments", formatter_class=SortingHelpFormatter)
+        super().__init__(description="This script inserts data into a mongo-powered database that stores book information", formatter_class=SortingHelpFormatter)
         # Positional arguments
         super().add_argument('-f','--fileName', type=str, help='EXCEL File where data is stored.')
         super().add_argument('-db', type=str, help='Select the DB to perform the operations')
